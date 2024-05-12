fb4467a6030a5e817a9e10e8306c835fa7200edf
Harshita222 <124767756+Harshita222@users.noreply.github.com>
2024-05-12 16:27:47 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
 //your JS code here. If required.
+const student ={
+	name:'harsh'
+}
+Object.prototype.getkeys()= function() {
+    // Use Object.keys() to get an array of keys
+    return Object.keys(this);
+};
