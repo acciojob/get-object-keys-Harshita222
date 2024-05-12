8d4fc87c0532f001b3dc6394662eb9d334e56c90
Harshita222 <124767756+Harshita222@users.noreply.github.com>
2024-05-12 16:31:28 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,19 @@
-//your JS code here. If required.
-const student ={
-	name:'harsh'
-}
-Object.prototype.getkeys()= function() {
-    // Use Object.keys() to get an array of keys
+
+const assert = require('assert');
+
+// Define the student object
+const student = {
+    name: "John"
+};
+
+// Add getKeys() method to Object prototype
+Object.prototype.getKeys = function() {
     return Object.keys(this);
 };
+
+describe('getKeys()', function() {
+    it('returns an array of all the keys in an object', function() {
+        const keys = student.getKeys();
+        assert.deepStrictEqual(keys, ['name']);
+    });
+});
